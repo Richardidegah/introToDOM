643793955402b2086608962e2f8dc7b33b3e27db
user <richardidegah7@gmail.com>
2025-04-29 12:20:05 +0000

    first commit

---
+++ dom.html/work.js
@@ -0,0 +1,76 @@
+//how to create an html element fully on .js using innerHTML
+// const body = document.body
+// const div = document.createElement('div')
+// div.innerHTML = 'to god be the glory'
+
+//using inner text
+// const strong = document.createElement('strong')
+// strong.innerText = 'the great africa'
+
+//another
+// const divTwo = document.createElement('divTwo')
+// divTwo.innerText = 'who are you'
+
+// body.append(div, divTwo, strong)
+
+
+//removing elements linked from the html 
+const body = document.body
+const div = document.querySelector('#div')
+const spanLove = document.querySelector('#love')
+const spanGod = document.querySelector('#god')
+
+// to remove
+ spanGod.remove()
+
+//to add
+// div.append(spanGod)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let body = document.body
+// let div = document.createElement('div')
+// // div.innerText = 'hello world'
+// div.textContent = 'hello world2'
+// body.append(div)
+
+// let h1 = document.createElement('h1')
+// h1.innerText = 'Nigeria is a grat country'
+// body.appendChild(h1)
+
+// // div.innerText = 'how are you'
+
+//
+// const body = document.body
+// const div = document.createElement('div')
+// div.innerHTML = '<strong> i lovr my country </strong>'
+// body.append(div)
